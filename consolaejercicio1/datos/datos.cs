690191291415bd0577aed5cff9824ca62882bd38
criseri23 <misselb624@gmail.com>
2026-09-01 20:25:20 -0300

    Refactor ProductoDatos to use MySQL database
    Updated ProductoDatos class to use MySQL for data operations, including methods for searching, adding, modifying, and deleting products.

--- consolaejercicio1/datos/datos.cs
+++ consolaejercicio1/datos/datos.cs
@@ -1,27 +1,113 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using MySql.Data.MySqlClient;
+using System;
 
 namespace Solucion_datos.datos
 {
     public class ProductoDatos
     {
- 
-        private List<(string Codigo, string Nombre, decimal Precio)> _tablaProductos =
-            new List<(string, string, decimal)>
-        {
-            ("PROD-101", "Teclado", 25000),
-            ("PROD-102", "Mouse", 15000),
-            ("PROD-103", "Monitor", 180000)
-        };
+        private string cadenaConexion =
+            "Server=localhost;Database=productosdb;Uid=root;Pwd=;";
 
         public (string Codigo, string Nombre, decimal Precio)? BuscarPorCodigo(string codigo)
         {
-            var resultado = _tablaProductos.FirstOrDefault(p => p.Codigo == codigo);
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
 
-            if (resultado.Codigo == null)
-                return null;
+                string consulta = "SELECT Codigo, Nombre, Precio FROM productos WHERE Codigo = @Codigo";
 
-            return resultado;
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Codigo", codigo);
+
+                    using (MySqlDataReader lector = comando.ExecuteReader())
+                    {
+                        if (lector.Read())
+                        {
+                            string codigoProducto = lector["Codigo"].ToString();
+                            string nombre = lector["Nombre"].ToString();
+                            decimal precio = Convert.ToDecimal(lector["Precio"]);
+
+                            return (codigoProducto, nombre, precio);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
+        public bool Agregar(string codigo, string nombre, decimal precio)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta =
+                    "INSERT INTO productos (Codigo, Nombre, Precio) " +
+                    "VALUES (@Codigo, @Nombre, @Precio)";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Codigo", codigo);
+                    comando.Parameters.AddWithValue("@Nombre", nombre);
+                    comando.Parameters.AddWithValue("@Precio", precio);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
+
+
+     
+        public bool Modificar(string codigo, string nombre, decimal precio)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta =
+                    "UPDATE productos " +
+                    "SET Nombre = @Nombre, Precio = @Precio " +
+                    "WHERE Codigo = @Codigo";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Codigo", codigo);
+                    comando.Parameters.AddWithValue("@Nombre", nombre);
+                    comando.Parameters.AddWithValue("@Precio", precio);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
+
+
+     
+        public bool Eliminar(string codigo)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta =
+                    "DELETE FROM productos WHERE Codigo = @Codigo";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Codigo", codigo);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
         }
     }
 }
+
