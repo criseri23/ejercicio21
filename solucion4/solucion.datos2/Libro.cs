464d63dc66d32f9a63ad24c483bbc93cef6a1acb
criseri23 <misselb624@gmail.com>
2026-09-01 20:37:56 -0300

    Enhance LibroDatos with CRUD operations
    Added methods to add, modify, and delete books in the database.

--- solucion4/solucion.datos2/Libro.cs
+++ solucion4/solucion.datos2/Libro.cs
@@ -1,45 +1,120 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 
 namespace solucion.datos2
 {
-        public class LibroDatos
+    public class LibroDatos
+    {
+        private string cadenaConexion =
+            "Server=localhost;Database=biblioteca;Uid=root;Pwd=;";
+
+
+     
+        public Libro BuscarPorIsbn(string isbn)
         {
-            private string cadenaConexion =
-                "Server=localhost;Database=biblioteca;Uid=root;Pwd=;";
+            Libro libro = null;
 
-            public Libro BuscarPorIsbn(string isbn)
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
             {
-                Libro libro = null;
+                conexion.Open();
 
-                using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
-                {
-                    conexion.Open();
-
-                    string consulta = @"SELECT isbn, titulo, autor, disponible
+                string consulta = @"SELECT isbn, titulo, autor, disponible
                                     FROM libros
                                     WHERE isbn = @isbn";
 
-                    using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
-                    {
-                        comando.Parameters.AddWithValue("@isbn", isbn);
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@isbn", isbn);
 
-                        using (MySqlDataReader lector = comando.ExecuteReader())
+                    using (MySqlDataReader lector = comando.ExecuteReader())
+                    {
+                        if (lector.Read())
                         {
-                            if (lector.Read())
-                            {
-                                libro = new Libro(
-                                    lector["isbn"].ToString(),
-                                    lector["titulo"].ToString(),
-                                    lector["autor"].ToString(),
-                                    Convert.ToBoolean(lector["disponible"])
-                                );
-                            }
+                            libro = new Libro(
+                                lector["isbn"].ToString(),
+                                lector["titulo"].ToString(),
+                                lector["autor"].ToString(),
+                                Convert.ToBoolean(lector["disponible"])
+                            );
                         }
                     }
                 }
-
-                return libro;
             }
-        
+
+            return libro;
+        }
+
+
+        public bool AgregarLibro(Libro libro)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta = @"INSERT INTO libros
+                                    (isbn, titulo, autor, disponible)
+                                    VALUES
+                                    (@isbn, @titulo, @autor, @disponible)";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@isbn", libro.Isbn);
+                    comando.Parameters.AddWithValue("@titulo", libro.Titulo);
+                    comando.Parameters.AddWithValue("@autor", libro.Autor);
+                    comando.Parameters.AddWithValue("@disponible", libro.Disponible);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
+
+
+        public bool ModificarLibro(Libro libro)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta = @"UPDATE libros
+                                    SET titulo = @titulo,
+                                        autor = @autor,
+                                        disponible = @disponible
+                                    WHERE isbn = @isbn";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@isbn", libro.Isbn);
+                    comando.Parameters.AddWithValue("@titulo", libro.Titulo);
+                    comando.Parameters.AddWithValue("@autor", libro.Autor);
+                    comando.Parameters.AddWithValue("@disponible", libro.Disponible);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
+
+
+        public bool EliminarLibro(string isbn)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta = @"DELETE FROM libros
+                                    WHERE isbn = @isbn";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@isbn", isbn);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
     }
 }
