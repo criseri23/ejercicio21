641f66b883ea0d8e74f2da9ff75713655721dd73
criseri23 <misselb624@gmail.com>
2026-09-01 20:26:42 -0300

    Refactor Datos class for MySQL integration
    Refactored Datos class to use MySQL database for student management, including methods for searching, adding, modifying, and deleting students.

--- solucion/ClassLibrary1/datos.cs
+++ solucion/ClassLibrary1/datos.cs
@@ -1,32 +1,116 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+using MySql.Data.MySqlClient;
 
 namespace solucion.datos
 {
     public class Datos
     {
-        private List<(int Legajo, string Nombre, string Condicion)> alumnos;
+        private string cadenaConexion =
+            "Server=localhost;Database=alumnosdb;Uid=root;Pwd=;";
 
-        public Datos()
-        {
-            alumnos = new List<(int, string, string)>
-            {
-                (10042, "Juan Perez", "Aprobado"),
-                (10043, "Maria Gomez", "Regular"),
-                (10044, "Lucas Fernandez", "Libre")
-            };
-        }
 
+       
         public (int Legajo, string Nombre, string Condicion)? BuscarAlumno(int legajo)
         {
-            var alumno = alumnos.FirstOrDefault(a => a.Legajo == legajo);
-
-            if (alumno.Legajo == 0)
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
             {
-                return null;
+                conexion.Open();
+
+                string consulta = @"SELECT legajo, nombre, condicion
+                                    FROM alumnos
+                                    WHERE legajo = @legajo";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@legajo", legajo);
+
+                    using (MySqlDataReader lector = comando.ExecuteReader())
+                    {
+                        if (lector.Read())
+                        {
+                            return (
+                                Convert.ToInt32(lector["legajo"]),
+                                lector["nombre"].ToString(),
+                                lector["condicion"].ToString()
+                            );
+                        }
+                    }
+                }
             }
 
-            return alumno;
+            return null;
+        }
+
+
+        
+        public bool AgregarAlumno(int legajo, string nombre, string condicion)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta = @"INSERT INTO alumnos
+                                    (legajo, nombre, condicion)
+                                    VALUES
+                                    (@legajo, @nombre, @condicion)";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@legajo", legajo);
+                    comando.Parameters.AddWithValue("@nombre", nombre);
+                    comando.Parameters.AddWithValue("@condicion", condicion);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
+
+
+        // MODIFICAR - UPDATE
+        public bool ModificarAlumno(int legajo, string nombre, string condicion)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta = @"UPDATE alumnos
+                                    SET nombre = @nombre,
+                                        condicion = @condicion
+                                    WHERE legajo = @legajo";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@legajo", legajo);
+                    comando.Parameters.AddWithValue("@nombre", nombre);
+                    comando.Parameters.AddWithValue("@condicion", condicion);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
+        }
+
+
+        public bool EliminarAlumno(int legajo)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+
+                string consulta = @"DELETE FROM alumnos
+                                    WHERE legajo = @legajo";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    comando.Parameters.AddWithValue("@legajo", legajo);
+
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    return filasAfectadas > 0;
+                }
+            }
         }
     }
 }
